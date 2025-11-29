b8194a536a8977b182dd2882b9c7c1e39b5300f0
themorguette <heyiamthemorguette@proton.me>
2025-11-29 06:12:45 +0300

    revised greetings.py and accounted for users not passing an arguement (flso)

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -5,22 +5,25 @@ class Greetings(commands.Cog):
         self.jarp = jarp
 
     @commands.command()
-    async def greetings(self, priop, flso):
-        FLSO = ['--hello', '-h', '--bye', '--goodbye', '-b', '-g']
+    async def greetings(self, priop, flso=None):
+        FLSO = ['--help', '--hello', '-h', '--goodbye', '--bye', '-g', '-b']
+        FLSO_hi = ['--hello', '-h']
+        FLSO_bye = ['--bye', '--goodbye', '-b', '-g']
         help_message = ("Allowed First Level Secondary Operations for the Primary Operation greetings: --hello/-h; --goodbye/--bye/-g/-b.\n"
-                        "ex: jarp greetings --hello *displays a greeting message of type hello.*")
-        help_FLSO = ['--help']
+                        "***(ex: jarp greetings --hello)***")
         help_string = "(Try: jarp greetings --help)"
 
         # user should enter: jarp greetings flso where flso is an element of FLSO
 
-        if flso not in FLSO and flso not in help_FLSO:
+        if not flso:
+            await priop.send(f"Please enter a First level Secondary Operation for the Primary operation greetings. {help_string}")
+        if flso not in FLSO:
             await priop.send(f"Please enter a valid First Level Secondary Operation for the Primary Operation greetings. {help_string}")
             return
-        if flso in FLSO[:2]:
+        if flso in FLSO_hi:
             await priop.send(f"Hello, {priop.author.mention}!")
             return
-        if flso in FLSO[2:]:
+        if flso in FLSO_bye:
             await priop.send(f"Goodbye, {priop.author.mention}!")
             return
         if flso in ['--help']:
